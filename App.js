22c39c16efa52e2df9d0e7a553653f385cd3634f
Jinu j s <jinu.js@firstbytedigital.com>
2018-08-07 17:35:31 +0530

    views added

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
- app.get('/',(req,res)=>{
-   res.send('Hello FirstByte');
- });
- app.get('/helloworld',(req,res)=>{
-   res.send('Hello world');
- });
+const path = require('path');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
 
  app.listen(3000,()=>console.log('listening on the port 3000..'));
